c20152b17d053030755730dae0bc01f179ff3f19
Jason Davidson <jdavidson@shujinko.io>
2020-12-01 20:59:27 -0800

    Add solution for day 1

--- src/AOC/Day01.hs
+++ src/AOC/Day01.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE TupleSections #-}
 module AOC.Day01 (day01) where
+
+import Safe (headMay, readMay)
 
 import AOC.Types
 day01 :: Solution [Int] Int Int
 day01 = Solution
-  { parse = const Nothing
-  , part1 = const Nothing
-  , part2 = const Nothing
+  { parse = traverse readMay . lines
+  , part1 = \xs -> headMay [x*y | x <- xs, y <- xs, x+y==2020]
+  , part2 = \xs -> headMay [x*y*z | x <- xs, y <- xs, z <- xs, x+y+z==2020]
   }
