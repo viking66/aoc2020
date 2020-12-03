de95d500ff6f7a8a2a811abf9dd72f171f446d96
Jason Davidson <jdavidson@shujinko.io>
2020-12-02 17:20:31 -0800

    add solution for day 2

--- src/AOC/Day02.hs
+++ src/AOC/Day02.hs
@@ -1,9 +1,49 @@
 module AOC.Day02 (day02) where
 
 import AOC.Types
-day02 :: Solution [Int] Int Int
+import Data.Maybe (fromMaybe)
+import Data.Void (Void)
+import Safe (atMay)
+import Text.Megaparsec (Parsec, many, parseMaybe)
+import Text.Megaparsec.Char (char, letterChar, newline, space1)
+import Text.Megaparsec.Char.Lexer (decimal)
+
+day02 :: Solution [PasswordTest] Int Int
 day02 = Solution
-  { parse = const Nothing
-  , part1 = const Nothing
-  , part2 = const Nothing
+  { parse = parseMaybe passwordTestListP
+  , part1 = Just . length . filter runPasswordCountTest
+  , part2 = Just . length . filter runPasswordIndexTest
   }
+
+data PasswordRule = PasswordRule Int Int Char
+  deriving Show
+
+data PasswordTest = PasswordTest PasswordRule String
+  deriving Show
+
+type Parser = Parsec Void String
+
+intP :: Parser Int
+intP = decimal
+
+passwordRuleP :: Parser PasswordRule
+passwordRuleP = PasswordRule <$> intP <* char '-' <*> intP <* space1 <*> letterChar <* char ':'
+
+passwordTestP :: Parser PasswordTest
+passwordTestP = PasswordTest <$> passwordRuleP <* space1 <*> many letterChar <* newline
+
+passwordTestListP :: Parser [PasswordTest]
+passwordTestListP = many passwordTestP
+
+runPasswordCountTest :: PasswordTest -> Bool
+runPasswordCountTest (PasswordTest (PasswordRule lo hi c) s) =
+  let n = length $ filter (==c) s
+  in lo <= n && n <= hi
+
+runPasswordIndexTest :: PasswordTest -> Bool
+runPasswordIndexTest (PasswordTest (PasswordRule lo hi c) s) = fromMaybe False $ do
+  x <- atMay' lo
+  y <- atMay' hi
+  Just $ (x==c) /= (y==c)
+  where
+    atMay' = atMay s . pred
--- test/AOC/Day02Spec.hs
+++ test/AOC/Day02Spec.hs
@@ -5,6 +5,15 @@ import AOC (day02, parse, part1, part2)
 
 spec :: Spec
 spec = do
-  describe "day02 ..." $ do
-    it "no-op" $ do
-      () `shouldBe` ()
+  describe "day02 part1" $ do
+    it "password test char count" $ do
+      go1 "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc\n" `shouldBe` Just 2
+  describe "day02 part2" $ do
+    it "password test index match" $ do
+      go2 "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc\n" `shouldBe` Just 1
+
+go1 :: String -> Maybe Int
+go1 = (part1 day02 =<<) . parse day02
+
+go2 :: String -> Maybe Int
+go2 = (part2 day02 =<<) . parse day02
