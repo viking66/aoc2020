1a06fab8513161633810a88efb3c3b2432cc2b89
Jason Davidson <jdavidson@shujinko.io>
2020-12-02 22:52:39 -0800

    add solution for day 3

--- src/AOC/Day03.hs
+++ src/AOC/Day03.hs
@@ -1,9 +1,43 @@
+{-# LANGUAGE LambdaCase #-}
 module AOC.Day03 (day03) where
 
 import AOC.Types
-day03 :: Solution [Int] Int Int
+import Data.List (transpose)
+import Safe (atMay)
+
+day03 :: Solution [[Content]] Int Int
 day03 = Solution
-  { parse = const Nothing
-  , part1 = const Nothing
-  , part2 = const Nothing
+  { parse = traverse (traverse parseContent) . lines
+  , part1 = treeProduct [(3, 1)]
+  , part2 = treeProduct [(1,1), (3,1), (5,1), (7,1), (1,2)]
   }
+
+data Content = Empty | Tree
+  deriving Show
+
+parseContent :: Char -> Maybe Content
+parseContent = \case
+  '.' -> Just Empty
+  '#' -> Just Tree
+  _   -> Nothing
+
+treeProduct :: [(Int, Int)] -> [[Content]] -> Maybe Int
+treeProduct slopes grid = fmap product . traverse slopeTreeCount $ slopes
+  where
+    slopeTreeCount :: (Int, Int) -> Maybe Int
+    slopeTreeCount (x, y) = slopeTreeCount' (cycle $ transpose grid) x (iterate (y+) y) 0
+
+    slopeTreeCount' :: [[Content]] -> Int -> [Int] -> Int -> Maybe Int
+    slopeTreeCount' _ _ [] _ = Nothing
+    slopeTreeCount' grid x (y:ys) n =
+      case splitAt x grid of
+        (_, a:as) ->
+          case atMay a y of
+            Nothing -> Just n
+            Just content -> slopeTreeCount' (a:as) x ys (treeIncrementer content n)
+        _         -> Nothing
+
+    treeIncrementer :: Content -> (Int -> Int)
+    treeIncrementer = \case
+      Empty -> id
+      Tree  -> succ
--- test/AOC/Day03Spec.hs
+++ test/AOC/Day03Spec.hs
@@ -5,6 +5,29 @@ import AOC (day03, parse, part1, part2)
 
 spec :: Spec
 spec = do
-  describe "day03 ..." $ do
-    it "no-op" $ do
-      () `shouldBe` ()
+  describe "day03 part1" $ do
+    it "tree count" $ do
+      go1 input `shouldBe` Just 7
+    it "tree product" $ do
+      go2 input `shouldBe` Just 336
+
+go1 :: String -> Maybe Int
+go1 = (part1 day03 =<<) . parse day03
+
+go2 :: String -> Maybe Int
+go2 = (part2 day03 =<<) . parse day03
+
+input :: String
+input = unlines
+  [ "..##......."
+  , "#...#...#.."
+  , ".#....#..#."
+  , "..#.#...#.#"
+  , ".#...##..#."
+  , "..#.##....."
+  , ".#.#.#....#"
+  , ".#........#"
+  , "#.##...#..."
+  , "#...##....#"
+  , ".#..#...#.#"
+  ]
