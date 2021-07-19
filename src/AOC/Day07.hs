9234888b720dfed373663fca59a5457449c030bf
Jason Davidson <jason.davidson@bitnomial.com>
2021-07-19 09:32:10 -0700

    day 7 solution

--- src/AOC/Day07.hs
+++ src/AOC/Day07.hs
@@ -1,9 +1,89 @@
+{-# LANGUAGE TupleSections #-}
+
 module AOC.Day07 (day07) where
 
 import AOC.Types
-day07 :: Solution [Int] Int Int
-day07 = Solution
-  { parse = const Nothing
-  , part1 = const Nothing
-  , part2 = const Nothing
-  }
+
+import Control.Monad.State (State, get, modify)
+import Data.Bifunctor (second)
+import Data.Bool (bool)
+import Data.Functor (($>))
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
+import Data.MultiSet (MultiSet)
+import qualified Data.MultiSet as MultiSet
+import Data.Void (Void)
+import Text.Megaparsec (Parsec, many, parseMaybe, parseTest, sepBy, (<|>))
+import Text.Megaparsec.Char (char, letterChar, newline, spaceChar, string)
+import Text.Megaparsec.Char.Lexer (decimal)
+import Data.Function.Memoize (memoize)
+
+day07 :: Solution RuleMap Int Int
+day07 =
+    Solution
+        { parse = parseMaybe ruleMapP
+        , part1 = Just . numContainingTarget target
+        , part2 = Just . totalBagsInTarget target
+        }
+
+data Bag = Bag String String
+    deriving (Show, Eq, Ord)
+
+unBag :: Bag -> (String, String)
+unBag (Bag s t) = (s, t)
+
+target :: Bag
+target = Bag "shiny" "gold"
+
+data Rules = Rules Bag (MultiSet Bag)
+
+newtype RuleMap = RuleMap {unRuleMap :: Map Bag (MultiSet Bag)}
+    deriving (Show)
+
+type Parser = Parsec Void String
+
+ruleMapP :: Parser RuleMap
+ruleMapP = RuleMap . Map.fromList . map unwrap <$> many rulesP
+  where
+    unwrap (Rules bag set) = (bag, set)
+
+rulesP :: Parser Rules
+rulesP = Rules <$> bagP <* string "bags contain " <*> bagSetP <* char '.' <* newline
+
+bagP :: Parser Bag
+bagP = Bag <$> many letterChar <* spaceChar <*> many letterChar <* spaceChar
+
+bagSetP :: Parser (MultiSet Bag)
+bagSetP = MultiSet.fromList <$> (emptySetP <|> nonEmptySetP)
+  where
+    emptySetP = string "no other bags" $> []
+    nonEmptySetP = concat <$> ruleP `sepBy` string ", "
+    ruleP = replicate <$> decimal <* spaceChar <*> bagP <* (string "bags" <|> string "bag")
+
+lookupBag :: Bag -> RuleMap -> MultiSet Bag
+lookupBag bag (RuleMap m) = fromMaybe mempty $ Map.lookup bag m
+
+numContainingTarget :: Bag -> RuleMap -> Int
+numContainingTarget target ruleMap = pred . snd . foldr foldAccum (mempty, 0) . Map.keys $ unRuleMap ruleMap
+  where
+    foldAccum :: Bag -> (Map Bag Bool, Int) -> (Map Bag Bool, Int)
+    foldAccum bag (m, n) = second (bool n $ succ n) $ containsBag bag m
+    containsBag :: Bag -> Map Bag Bool -> (Map Bag Bool, Bool)
+    containsBag b m
+        | b == target = (m, True)
+        | otherwise = maybe (containsBagRecur b m) (m,) $ Map.lookup b m
+    containsBagRecur :: Bag -> Map Bag Bool -> (Map Bag Bool, Bool)
+    containsBagRecur b m =
+        foldr f (m, False)
+            . MultiSet.distinctElems
+            $ lookupBag b ruleMap
+      where
+        f :: Bag -> (Map Bag Bool, Bool) -> (Map Bag Bool, Bool)
+        f bag (m, b) = let (m', b') = containsBag bag m in (Map.insert bag b' m', b || b')
+
+totalBagsInTarget :: Bag -> RuleMap -> Int
+totalBagsInTarget target (RuleMap m) = pred . memoize go $ unBag target
+  where
+    go (s,t) = succ . maybe 0 recurSet $ Map.lookup (Bag s t) m
+    recurSet = sum . map (go . unBag) . MultiSet.toList
