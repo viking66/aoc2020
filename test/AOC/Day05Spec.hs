704b17c63d2b50a886ada0d80f8de795d302b1cd
Jason Davidson <jason.davidson@bitnomial.com>
2021-07-28 09:55:44 -0700

    add test for day 5

--- test/AOC/Day05Spec.hs
+++ test/AOC/Day05Spec.hs
@@ -5,6 +5,9 @@ import AOC (day05, parse, part1, part2)
 
 spec :: Spec
 spec = do
-  describe "day05 ..." $ do
-    it "no-op" $ do
-      () `shouldBe` ()
+  describe "day05 part1" $ do
+    it "calculate seat ID" $ do
+      go1 "FBFBBFFRLR" `shouldBe` Just 357
+
+go1 :: String -> Maybe Int
+go1 = (part1 day05 =<<) . parse day05
