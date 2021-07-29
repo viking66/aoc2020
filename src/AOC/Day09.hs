ae13effb6f838a8b95560e7c59911724a37a6360
Jason Davidson <jason.davidson@bitnomial.com>
2021-07-29 11:04:06 -0700

    add day 9 solution

--- src/AOC/Day09.hs
+++ src/AOC/Day09.hs
@@ -1,9 +1,66 @@
-module AOC.Day09 (day09) where
+{-# LANGUAGE NamedFieldPuns #-}
+module AOC.Day09 (XMAS, day09, mkXMAS) where
 
 import AOC.Types
-day09 :: Solution [Int] Int Int
+
+import Control.Monad ((<=<))
+import Data.List (find, tails, scanl')
+import Data.MultiSet (MultiSet)
+import qualified Data.MultiSet as MultiSet
+import Safe (headMay, maximumMay, minimumMay, readMay)
+
+day09 :: Solution XMAS Int Int
 day09 = Solution
-  { parse = const Nothing
-  , part1 = const Nothing
-  , part2 = const Nothing
+  { parse = fmap (mkXMAS 25) . traverse readMay . lines
+  , part1 = missingSum
+  , part2 = \xmas -> (`encryptionWeakness` nums xmas) =<< missingSum xmas
   }
+
+data XMAS = XMAS
+    { operands :: MultiSet Int
+    , nums :: [Int]
+    , targets :: [Int]
+    }
+
+mkXMAS :: Int -> [Int] -> XMAS
+mkXMAS n ns = XMAS
+    { operands = MultiSet.fromList xs
+    , nums = ns
+    , targets = ys
+    }
+  where
+    (xs, ys) = splitAt n ns
+
+nextTarget :: XMAS -> XMAS
+nextTarget xmas@XMAS{operands, nums, targets}
+    | null targets = xmas
+    | null nums = XMAS{operands = operands, nums = nums, targets = targets'}
+    | otherwise =
+        let (operands', nums') = shift operands nums
+        in XMAS{operands = operands', nums = nums', targets = targets'}
+  where
+    targets' = drop 1 targets
+    shift o [] = (o, [])
+    shift o (n:ns) = (addTarget $ MultiSet.delete n o, ns)
+    addTarget o = maybe o (`MultiSet.insert` o) (headMay targets)
+
+missingSum :: XMAS -> Maybe Int
+missingSum xmas@XMAS{operands, nums, targets} = case targets of
+    [] -> Nothing
+    (t:ts) -> if hasPairSum t operands
+              then missingSum (nextTarget xmas)
+              else Just t
+
+hasPairSum :: Int -> MultiSet Int -> Bool
+hasPairSum target nums = any f (MultiSet.distinctElems nums)
+  where
+    f n = let m = target - n in MultiSet.member m nums && n /= m
+
+encryptionWeakness :: Int -> [Int] -> Maybe Int
+encryptionWeakness n = minMaxSum . trim <=< prefixSum
+  where
+    prefixSum = find targetSum . map (drop 1 . scanl' sums (0,0)) . tails
+    sums (_, s) m = (m, s+m)
+    targetSum = elem n . map snd
+    trim = map fst . takeWhile ((<=n) . snd)
+    minMaxSum xs = (+) <$> minimumMay xs <*> maximumMay xs
--- test/AOC/Day09Spec.hs
+++ test/AOC/Day09Spec.hs
@@ -1,10 +1,23 @@
 module AOC.Day09Spec (spec) where
 
 import Test.Hspec
-import AOC (day09, parse, part1, part2)
+import AOC (XMAS, day09, mkXMAS, parse, part1, part2)
 
 spec :: Spec
 spec = do
-  describe "day09 ..." $ do
-    it "no-op" $ do
-      () `shouldBe` ()
+  describe "day09 part1" $ do
+    it "find missing sum number" $ do
+      go1 input `shouldBe` Just 127
+  describe "day09 part2" $ do
+    it "find encryption weakness" $ do
+      go2 input `shouldBe` Just 62
+
+go1 :: XMAS -> Maybe Int
+go1 = part1 day09
+
+go2 :: XMAS -> Maybe Int
+go2 = part2 day09
+
+input :: XMAS
+input = mkXMAS
+    5 [35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576]
