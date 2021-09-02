1878cb323b6db0d86314ee8ccf1422ed82b596a2
Jason Davidson <jason.davidson@bitnomial.com>
2021-09-02 15:09:46 -0700

    day 13 solution

--- src/AOC/Day13.hs
+++ src/AOC/Day13.hs
@@ -1,9 +1,44 @@
+{-# LANGUAGE BangPatterns #-}
+
 module AOC.Day13 (day13) where
 
+import Data.Function (on)
+import Data.List (find, foldl', group)
+import Data.List.Split (splitOn)
+import Safe (maximumByMay, minimumMay, readMay)
+
 import AOC.Types
-day13 :: Solution [Int] Int Int
-day13 = Solution
-  { parse = const Nothing
-  , part1 = const Nothing
-  , part2 = const Nothing
-  }
+
+day13 :: Solution Schedule Int Int
+day13 =
+    Solution
+        { parse = toSchedule
+        , part1 = part1'
+        , part2 = part2'
+        }
+
+data Schedule = Schedule Int [Bus]
+
+data Bus = Bus Int Int
+
+toSchedule :: String -> Maybe Schedule
+toSchedule = go . lines
+  where
+    go [t, s] = Schedule <$> readMay t <*> toBuses s
+    go _ = Nothing
+    toBuses = traverse toBus . filter (("x" /=) . snd) . zip [0 ..] . splitOn ","
+    toBus (i, s) = Bus i <$> readMay s
+
+part1' :: Schedule -> Maybe Int
+part1' (Schedule t bs) = fmap (uncurry (*)) . minimumMay $ map f bs
+  where
+    f (Bus _ b) = (b - (t `mod` b), b)
+
+-- I got stuck on this and didn't feel like learning the Chinese Remainder Theorem so I copied a solution from
+-- https://github.com/mstksg/advent-of-code-2020/blob/master/reflections-out/day13.md
+part2' :: Schedule -> Maybe Int
+part2' (Schedule _ bs) = Just . fst $ foldl' go (0, 1) bs
+  where
+    go (!base, !step) (Bus i s) = (base', step * s)
+      where
+        base' = until (\n -> (n + i) `mod` s == 0) (+ step) base
--- test/AOC/Day13Spec.hs
+++ test/AOC/Day13Spec.hs
@@ -1,10 +1,28 @@
 module AOC.Day13Spec (spec) where
 
+import Control.Monad ((<=<))
 import Test.Hspec
+
 import AOC (day13, parse, part1, part2)
 
 spec :: Spec
 spec = do
-  describe "day13 ..." $ do
-    it "no-op" $ do
-      () `shouldBe` ()
+    describe "day13 part1" $ do
+        it "Earliest bus" $ do
+            go1 input `shouldBe` Just 295
+    describe "day13 part2" $ do
+        it "All times match" $ do
+            go2 input `shouldBe` Just 1068781
+
+go1 :: String -> Maybe Int
+go1 = part1 day13 <=< parse day13
+
+go2 :: String -> Maybe Int
+go2 = part2 day13 <=< parse day13
+
+input :: String
+input =
+    unlines
+        [ "939"
+        , "7,13,x,x,59,x,31,19"
+        ]
