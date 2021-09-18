1864c82c0f4e074b2c63eabfdf5d01043259c051
Jason Davidson <jason.davidson@bitnomial.com>
2021-09-18 14:14:36 -0700

    Add day 15 solution

--- src/AOC/Day15.hs
+++ src/AOC/Day15.hs
@@ -1,9 +1,50 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module AOC.Day15 (day15) where
 
+import Data.List (find)
+import Data.List.Split (splitOn)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Safe (initMay, lastMay, readMay)
+
 import AOC.Types
-day15 :: Solution [Int] Int Int
-day15 = Solution
-  { parse = const Nothing
-  , part1 = const Nothing
-  , part2 = const Nothing
-  }
+
+day15 :: Solution GameState Int Int
+day15 =
+    Solution
+        { parse = parseGameState
+        , part1 = play 2020
+        , part2 = play 30000000
+        }
+
+data GameState = GameState
+    { number :: Int
+    , index :: Int
+    , history :: Map Int Int
+    }
+    deriving (Show)
+
+parseGameState :: String -> Maybe GameState
+parseGameState s = do
+    xs <- traverse readMay $ splitOn "," s
+    (xs', x) <- splitLast xs
+    pure
+        GameState
+            { number = x
+            , index = length xs
+            , history = Map.fromList $ zip xs' [1 ..]
+            }
+  where
+    splitLast xs = (,) <$> initMay xs <*> lastMay xs
+
+play :: Int -> GameState -> Maybe Int
+play n = nthNumber n . iterate nextNumber
+  where
+    nthNumber n = fmap number . find ((n ==) . index) . take n
+    nextNumber GameState{number, index, history} =
+        GameState
+            { number = maybe 0 (index -) $ Map.lookup number history
+            , index = succ index
+            , history = Map.insert number index history
+            }
--- test/AOC/Day15Spec.hs
+++ test/AOC/Day15Spec.hs
@@ -1,10 +1,18 @@
 module AOC.Day15Spec (spec) where
 
+import Control.Monad ((<=<))
 import Test.Hspec
+
 import AOC (day15, parse, part1, part2)
 
 spec :: Spec
 spec = do
-  describe "day15 ..." $ do
-    it "no-op" $ do
-      () `shouldBe` ()
+    describe "day15 part1" $ do
+        it "2020th number" $ do
+            go1 input `shouldBe` Just 436
+
+go1 :: String -> Maybe Int
+go1 = part1 day15 <=< parse day15
+
+input :: String
+input = "0,3,6"
